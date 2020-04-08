7deb099fa3c33969bff1772b69574f752f20ed09
Josepheng Xiong <jsxiong@ucsc.edu>
2020-04-08 15:47:19 -0700

    Scenes * assetts committed

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,4 +2,11 @@ class Menu extends Phaser.Scene {
     constructor() {
         super("menuScene");
     }
+    create() {
+        // display menu text
+        this.add.text(20, 20, "Rocket Patrol Menu");
+
+        // launch next scene
+        this.scene.start("playScene");
+    }
 }
